187ad55fa09c5b21fa863eaa5b29419b136d75d6
maximilianor-tkf <71152032+maximilianor-tkf@users.noreply.github.com>
2023-01-05 11:24:53 -0300

    Update authConfig.js

--- App/authConfig.js
+++ App/authConfig.js
@@ -11,7 +11,7 @@ const msalConfig = {
       clientId: "de88aca4-bad6-4b4a-b2e9-2f2d9a97472c", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
-      redirectUri: "http://localhost:6420", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
+      redirectUri: "https://ms-identity-b2c-javascript-spa-comercios.azurewebsites.net", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
     },
     cache: {
       cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
